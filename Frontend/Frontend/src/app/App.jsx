62a5d2e5911120064272b046f5d7274545bd9fb3
Gayatri Rathore <gaytrirathore314@gmail.com>
2026-07-24 12:34:38 +0530

    fix react router provider

--- Frontend/Frontend/src/app/App.jsx
+++ Frontend/Frontend/src/app/App.jsx
@@ -1,21 +1,21 @@
-import { RouterProvider } from "react-router"
-import { router } from "./app.routes"
-import { useAuth } from "../features/auth/hook/UseAuth"
-import { useEffect } from "react"
+  import { RouterProvider } from "react-router"
+  import { router } from "./app.routes"
+  import { useAuth } from "../features/auth/hook/UseAuth"
+  import { useEffect } from "react"
 
-function App() {
- 
+  function App() {
+  
 
-  const auth=useAuth()
+    const auth=useAuth()
 
-  useEffect(()=>{
-    auth.handlegetMe()
-  },[])
+    useEffect(()=>{
+      auth.handlegetMe()
+    },[])
 
-  return (
-    <><RouterProvider router={router}/></>
-      
-  )
-}
+    return (
+      <><RouterProvider router={router}/></>
+        
+    )
+  }
 
-export default App
+  export default App
--- Frontend/Frontend/src/main.jsx
+++ Frontend/Frontend/src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './app/index.css'
 import App from './app/App.jsx'
 import { store } from './app/app.store.js'
-import {Provider } from 'react-redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
-  
+  <BrowserRouter>
     <Provider store={store}>
-       <App />
+      <App />
     </Provider>
-   
-  
+  </BrowserRouter>
 )
+
